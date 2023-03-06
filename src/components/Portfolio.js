39a3070af88b27e1c2ee0ec3d5bda58720cebd3a
Zahir Ali <zahirali@Zahirs-MacBook-Air.local>
2023-03-05 20:57:33 -0800

    another commit

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,7 @@ const Portfolio = () => {
     return (
         <>
         <div className="absolute top-[20px] left-[20px] md:top-[60px] md:left-[100px]">
-        <h1 className="drop-shadow-md text-primary md:text-[90px]"><span className="text-[30px] font-normal">Hi,</span><br />I am Zahir</h1>
+        <h1 className="drop-shadow-md text-primary md:text-[90px]"><br />I am Zahir</h1>
         <p className="text-white mt-8 text-[20px]">A designer from Vancouver</p>
       </div>
       
